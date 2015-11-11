370774c7fec48a20c324fe54a05e03733d7c46c6
Luis Leiva <llt@acm.org>
2015-11-11 18:35:14 +0100

    Made input sanitize more explicit

--- wp-mucaptcha/admin/save.php
+++ wp-mucaptcha/admin/save.php
@@ -1,11 +1,8 @@
 <?php
-// Clean user input before further processing.
-$_POST = array_map('sanitize_text_field', $_POST);
-
-$public_key = $_POST['mucaptcha_public_key'];
+$public_key = sanitize_text_field( $_POST['mucaptcha_public_key'] );
 update_option( 'mucaptcha_public_key', $public_key );
 
-$secret_key = $_POST['mucaptcha_secret_key'];
+$secret_key = sanitize_text_field( $_POST['mucaptcha_secret_key'] );
 update_option( 'mucaptcha_secret_key', $secret_key );
 
 $in_comment = isset($_POST['mucaptcha_in_comment_form']) ?
@@ -20,7 +17,8 @@ if ( !is_null( $in_comment_logged ) ) {
   update_option( 'mucaptcha_in_comment_form_logged', $in_comment_logged );
 }
 
-$in_login = !empty($_POST['mucaptcha_in_login_form']);
+$in_login = isset($_POST['mucaptcha_in_login_form']) ?
+                  $_POST['mucaptcha_in_login_form'] : NULL;
 if ( !is_null( $in_login ) ) {
   update_option( 'mucaptcha_in_login_form', $in_login );
 }
@@ -31,7 +29,7 @@ if ( !is_null( $in_register_form ) ) {
   update_option( 'mucaptcha_in_register_form', $in_register );
 }
 
-$display_text = $_POST['mucaptcha_display_text'];
+$display_text = sanitize_text_field( $_POST['mucaptcha_display_text'] );
 update_option( 'mucaptcha_display_text', $display_text );
 
 $touchonly = isset($_POST['mucaptcha_touchonly']) ?
@@ -40,15 +38,15 @@ if ( !is_null( $touchonly ) ) {
   update_option( 'mucaptcha_touchonly', $touchonly );
 }
 
-$line_color = $_POST['mucaptcha_line_color'];
+$line_color = sanitize_text_field( $_POST['mucaptcha_line_color'] );
 update_option( 'mucaptcha_line_color', $line_color );
 
-$line_width = intval( $_POST['mucaptcha_line_width'] );
+$line_width = intval( sanitize_text_field( $_POST['mucaptcha_line_width'] ) );
 if ( $line_width > 0 ) {
   update_option( 'mucaptcha_line_width', $line_width );
 }
 
-$background_color = $_POST['mucaptcha_background_color'];
+$background_color = sanitize_text_field( $_POST['mucaptcha_background_color'] );
 update_option( 'mucaptcha_background_color', $background_color );
 ?>
 
