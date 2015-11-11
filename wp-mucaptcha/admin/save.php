dd6c454bbda1650f90045349de4e5871c9468172
Luis Leiva <llt@acm.org>
2015-11-11 18:27:42 +0100

    Check for non-null values before saving options

--- wp-mucaptcha/admin/save.php
+++ wp-mucaptcha/admin/save.php
@@ -10,32 +10,43 @@ update_option( 'mucaptcha_secret_key', $secret_key );
 
 $in_comment = isset($_POST['mucaptcha_in_comment_form']) ?
                     $_POST['mucaptcha_in_comment_form'] : NULL;
-update_option( 'mucaptcha_in_comment_form', $in_comment );
+if ( !is_null( $in_comment ) ) {
+  update_option( 'mucaptcha_in_comment_form', $in_comment );
+}
 
 $in_comment_logged = isset($_POST['mucaptcha_in_comment_form_logged']) ?
                            $_POST['mucaptcha_in_comment_form_logged'] : NULL;
-update_option( 'mucaptcha_in_comment_form_logged', $in_comment_logged );
+if ( !is_null( $in_comment_logged ) ) {
+  update_option( 'mucaptcha_in_comment_form_logged', $in_comment_logged );
+}
 
-$in_login = isset($_POST['mucaptcha_in_login_form']) ?
-                  $_POST['mucaptcha_in_login_form'] : NULL;
-update_option( 'mucaptcha_in_login_form', $in_login );
+$in_login = !empty($_POST['mucaptcha_in_login_form']);
+if ( !is_null( $in_login ) ) {
+  update_option( 'mucaptcha_in_login_form', $in_login );
+}
 
 $in_register = isset($_POST['mucaptcha_in_register_form']) ?
                      $_POST['mucaptcha_in_register_form'] : NULL;
-update_option( 'mucaptcha_in_register_form', $in_register );
+if ( !is_null( $in_register_form ) ) {
+  update_option( 'mucaptcha_in_register_form', $in_register );
+}
 
 $display_text = $_POST['mucaptcha_display_text'];
 update_option( 'mucaptcha_display_text', $display_text );
 
 $touchonly = isset($_POST['mucaptcha_touchonly']) ?
                    $_POST['mucaptcha_touchonly'] : NULL;
-update_option( 'mucaptcha_touchonly', $touchonly );
+if ( !is_null( $touchonly ) ) {
+  update_option( 'mucaptcha_touchonly', $touchonly );
+}
 
 $line_color = $_POST['mucaptcha_line_color'];
 update_option( 'mucaptcha_line_color', $line_color );
 
-$line_width = $_POST['mucaptcha_line_width'];
-update_option( 'mucaptcha_line_width', $line_width );
+$line_width = intval( $_POST['mucaptcha_line_width'] );
+if ( $line_width > 0 ) {
+  update_option( 'mucaptcha_line_width', $line_width );
+}
 
 $background_color = $_POST['mucaptcha_background_color'];
 update_option( 'mucaptcha_background_color', $background_color );
